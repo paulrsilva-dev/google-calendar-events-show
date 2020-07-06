c20c8d4bb7dd201f69a4bf00cb1594618306a47a
ruslankoz <ruslankozubovskyi@gmail.com>
2020-07-05 21:38:03 -0400

    Deleted custom functions for format date and time

--- src/common/index.js
+++ src/common/index.js
@@ -1,18 +1,3 @@
-export const formatDate = timestamp => {
-  const day = timestamp.slice(8, 10);
-  const month = timestamp.slice(5, 7);
-  const year = timestamp.slice(0, 4)
-
-  return { day, month, year };
-};
-
-export const formatTime = timestamp => {
-  const hour = timestamp.slice(11, 13);
-  const minute = timestamp.slice(14, 16);
-
-  return { hour, minute };
-};
-
 export const checkInputDates = (start, end) => {
   if (start > end) return false;
   return true;
